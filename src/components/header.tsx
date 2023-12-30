8ca1a0bf10eea76e2f02844a35d9e2ce08b98fdb
RAHUL <dasr16583@gmail.com>
2023-12-30 09:20:54 +0530

    working done on header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,32 +11,41 @@ import { Link } from "react-router-dom";
 const user = { _id: "abc", role: "admin" };
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const [isOpen,setIsOpen] = useState<boolean>(false)
+  const logoutHandler = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/search"}>
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
         <FaSearch />
       </Link>
-      <Link to={"/cart"}>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
         <FaShoppingBag />
       </Link>
 
       {user?._id ? (
         <>
-          <button onClick={()=>setIsOpen((prev)=>!prev)}>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
             <FaUser />
           </button>
 
           <dialog open={isOpen}>
             <div>
               {user.role === "admin" && (
-                <Link to={"/admin/dashboard"}>Admin</Link>
+                <Link onClick={() => setIsOpen(false)} to={"/admin/dashboard"}>
+                  Admin
+                </Link>
               )}
-              <Link to={"/orders"}>Orders</Link>
-              <button>
+              <Link onClick={() => setIsOpen(false)} to={"/orders"}>
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
                 <FaSignOutAlt />
               </button>
             </div>
