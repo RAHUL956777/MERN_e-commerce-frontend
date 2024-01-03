539e3b559597bb595abacdd513df7871eb7eb4fc
RAHUL <dasr16583@gmail.com>
2024-01-03 20:14:03 +0530

    working done on search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ProductCard from "../components/product-card";
 
 const Search = () => {
   const [search, setSearch] = useState("");
@@ -6,6 +7,11 @@ const Search = () => {
   const [maxPrice, setMaxPrice] = useState<number>(0);
   const [category, setCategory] = useState("");
   const [page, setPage] = useState(1);
+
+  const addToCartHandler = () => {};
+
+  const isPrevPage = page > 1;
+  const isNextPage = page < 4;
 
   return (
     <div className="product-search-page">
@@ -44,15 +50,40 @@ const Search = () => {
       </aside>
       <main>
         <h1>Products</h1>
-        <input type="text"
-        placeholder="Search by name..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
 
         <div className="search-product-list">
-          
+          <ProductCard
+            productId="1"
+            name="mobile"
+            price={10005}
+            stock={200}
+            handler={addToCartHandler}
+            photo="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/1/i/x/-original-imagtc6fhhtqjnr9.jpeg?q=70"
+          />
         </div>
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Previous
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </article>
       </main>
     </div>
   );
