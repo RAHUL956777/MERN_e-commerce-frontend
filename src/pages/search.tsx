235d697cb3c62f70467e1a79c3101e2236029ba6
RAHUL <dasr16583@gmail.com>
2024-01-03 19:46:18 +0530

    working on search components

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { useState } from "react";
 
 const Search = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState<number>(0);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price(Low to High)</option>
+            <option value="dsc">Price( High to Low)</option>
+          </select>
+        </div>
+        <div>
+          <h4>Max Price {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">Sample1</option>
+            <option value="">Sample2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input type="text"
+        placeholder="Search by name..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Search;
