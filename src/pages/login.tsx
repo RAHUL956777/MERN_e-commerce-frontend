09c58ec5c64d919649050fb978eb0367be7ac25c
RAHUL <dasr16583@gmail.com>
2024-01-24 10:57:49 +0530

    workin done on user login wuth google

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,15 +3,38 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "../firebase.ts";
+import { useLoginMutation } from "../redux/api/userAPI.ts";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { MessageResponse } from "../types/api-types.ts";
 
 const Login = () => {
   const [gender, setGender] = useState("");
   const [date, setDate] = useState("");
 
+  const [login] = useLoginMutation();
+
   const loginHandler = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const { user } = await signInWithPopup(auth, provider);
+
+      const res = await login({
+        name: user.displayName!,
+        email: user.email!,
+        photo: user.photoURL!,
+        gender,
+        role: "user",
+        dob: date,
+        _id: user.uid,
+      });
+
+      if ("data" in res) {
+        toast.success(res.data.message);
+      } else {
+        const error = res.error as FetchBaseQueryError;
+        const message = error.data as MessageResponse;
+        toast.error(message.message);
+      }
       console.log(user);
     } catch (error) {
       console.log(error);
